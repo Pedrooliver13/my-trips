da6788540c3f0de91fa5a60293901abd51699657
pedrooliver13 <pedro@qyon.com>
2021-08-20 18:38:42 -0300

    add template 'about' and 'graphQL'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
 import dynamic from 'next/dynamic';
+import { InfoOutline } from '@styled-icons/evaicons-outline';
+
+import LinkWrapper from 'components/LinkWrapper';
 
 const Map = dynamic(() => import('components/Map'), { ssr: false });
 
 export default function Home() {
-  const place = [
-    {
-      id: 1,
-      name: 'nota pontiguá',
-      slug: 'pontiguá',
-      location: {
-        latitude: 129,
-        longitude: -50
-      }
-    }
-  ];
-
-  return <Map places={place} />;
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} arial-label="About" />
+      </LinkWrapper>
+      <Map />;
+    </>
+  );
 }
